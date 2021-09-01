9ca7ab3f21ba15a3dd27eb098b415952b54ca15e
Max Kovalenko <tutmrmaxi@gmail.com>
2021-09-01 20:33:50 +0300

    feat: add guessing game code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,51 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.minNumb = min;
+    this.maxNumb = max;
+  }
 
-    }
+  guess() {
+    this.guessed = Math.ceil((this.minNumb + this.maxNumb) / 2);
+    return this.guessed;
+  }
 
-    guess() {
+  lower() {
+    this.maxNumb = this.guessed;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.minNumb = this.guessed;
+  }
 }
 
 module.exports = GuessingGame;
+
+const number = 998;
+const game = new GuessingGame();
+game.setRange(0, 6118);
+
+let result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
